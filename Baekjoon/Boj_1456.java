1946a84447c85a6d94da0c2f353dd3aff87d7ca0
chocoding <galmeagi2@naver.com>
2024-12-11 12:06:42 +0900

    거의 소수

--- Baekjoon/Boj_1456.java
+++ Baekjoon/Boj_1456.java
@@ -20,9 +20,10 @@ public class Boj_1456 {
         A = Long.parseLong(st.nextToken());
         B = Long.parseLong(st.nextToken());
 
-        sosu = new boolean[limit + 1];
+        sosu = new boolean[limit + 1]; //10^7승까지만 소수를 구해도 된다.
+        // 그 이후의 소수를 구해도 거의 소수를 구하기 위해 제곱을 하면 10^14승을 넘어가버리기 때문
 
-        erato();
+        erato(); // 소수 판별
 
         System.out.println(finalCal());
     }
@@ -30,14 +31,15 @@ public class Boj_1456 {
     private static int finalCal() {
         int cnt = 0;
 
-        for (int i = 1; i < Math.sqrt(B) + 1; i++) {
+        for (int i = 1; i < limit + 1; i++) {
             if (!sosu[i]) {
-                long j = i * i;
-                while (j < B + 1) {
-                    if (A <= j) {
-                        cnt += 1;
+                long curSosu = i;
+                int k = 1;
+                while (curSosu <= B / Math.pow(curSosu, k)) {
+                    if ( A / Math.pow(curSosu, k) <= curSosu) {
+                        cnt++;
                     }
-                    j *= i;
+                    k++;
                 }
             }
         }
@@ -46,7 +48,7 @@ public class Boj_1456 {
 
     private static void erato() {
         sosu[0] = sosu[1] = true;
-        for (int i = 2; i < Math.sqrt(B) + 1; i++) {
+        for (int i = 2; i < Math.sqrt(limit) + 1; i++) {
             if (!sosu[i]) {
                 for (int j = i * i; j < limit + 1; j += i) {
                     sosu[j] = true;
