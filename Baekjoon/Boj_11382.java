f268ccd883f87b8f260210cec376502e2221812f
chocoding <galmeagi2@naver.com>
2025-02-03 15:09:53 +0900

    꼬마 정민

--- Baekjoon/Boj_11382.java
+++ Baekjoon/Boj_11382.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Boj_11382 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_11382 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        System.out.println(A + B + C);
+    }
 }
