4cb1f5110dd7772bdf63312e36577b5fc0027246
chocoding <galmeagi2@naver.com>
2025-02-03 15:27:23 +0900

    알람 시계

---
+++ Baekjoon/Boj_2884.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_2884 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if (M >= 45) {
+            M = M - 45;
+        } else {
+            M = 60 + (M - 45);
+            if (H == 0) {
+                H = 23;
+            } else {
+                H--;
+            }
+        }
+        System.out.println(H + " " + M);
+
+    }
+}
