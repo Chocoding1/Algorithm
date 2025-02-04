6c2d75ccebb6ac39f285a4f3c60a054c2913f7bd
chocoding <galmeagi2@naver.com>
2025-02-04 14:53:31 +0900

    아스키 코드

---
+++ Baekjoon/Boj_11654.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_11654 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int c = br.readLine().charAt(0);
+        System.out.println(c);
+    }
+}
