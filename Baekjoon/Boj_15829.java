4fbcb2c530023a493d014f49afa7a7a2abf9c4e9
chocoding <galmeagi2@naver.com>
2025-02-13 10:25:20 +0900

    Hashing

--- Baekjoon/Boj_15829.java
+++ Baekjoon/Boj_15829.java
@@ -9,34 +9,19 @@ import java.io.InputStreamReader;
  */
 public class Boj_15829 {
 
-//    static final int r = 31;
+    static final int r = 31;
     static final int M = 1234567891;
-
-    // 내가 처음 푼 풀이(왜 틀린지 아직 못 찾음)
-/*
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int L = Integer.parseInt(br.readLine());
         char[] str = br.readLine().toCharArray();
 
         long result = 0;
+        long powValue = 1;
         for (int i = 0; i < L; i++) {
-            result += (long) (((int) str[i] - 96) * Math.pow(r, i) % M) % M;
-        }
-
-        System.out.println(result % M);
-    }
-*/
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int L = Integer.parseInt(br.readLine());
-        char[] str = br.readLine().toCharArray();
-
-        long result = 0;
-        long r = 1;
-        for (int i = 0; i < L; i++) {
-            result += (((int) str[i] - 96) * r) % M;
-            r = (r * 31) % M;
+            result += (((int) str[i] - 96) * powValue) % M;
+            powValue = (powValue * r) % M;
         }
 
         System.out.println(result % M);
