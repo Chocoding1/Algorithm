c5a4f7586e1613eef107c9cb666b8239c2190e3d
chocoding <galmeagi2@naver.com>
2025-05-10 10:00:36 +0900

    N과 M(9)

---
+++ Baekjoon/Boj_15654.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj_15654 {
+
+    private static int N, M;
+    private static int[] seq, num;
+    private static boolean[] visited;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        seq = new int[M];
+        num = new int[N];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int n) {
+        if (n == M) {
+            for (int i : seq) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            seq[n] = num[i];
+            visited[i] = true;
+            dfs(n + 1);
+            visited[i] = false;
+        }
+    }
+}
